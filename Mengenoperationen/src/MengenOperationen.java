82a46a67dd6111c9c2b6918cd29c86f05a6c8082
Happy-Wiggles <34844347+Happy-Wiggles@users.noreply.github.com>
2019-04-12 17:10:57 +0200

    Added differentiation of operators

--- Mengenoperationen/src/MengenOperationen.java
+++ Mengenoperationen/src/MengenOperationen.java
@@ -1,52 +1,84 @@
 import java.util.Scanner;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MengenOperationen {
+
+    //Schnitt (*)
+    private static Set schnittMenge(Set setA, Set setB){
+        Set <Integer> schnitt = new HashSet<Integer>(setA);
+        schnitt.retainAll(setB);
+        return schnitt;
+    }
+    //Differenz (-)
+    private static Set differenzMenge(Set setA, Set setB){
+        Set <Integer> differenz = new HashSet<Integer>(setA);
+        differenz.removeAll(setB);
+        return differenz;
+    }
+    //Vereinigung (+)
+    private static Set vereinigungsMenge(Set setA, Set setB){
+        Set <Integer> vereinigung = new HashSet<Integer>(setA);
+        vereinigung.addAll(setB);
+        return vereinigung;
+    }
 
     public static void main(String [] args){
 
         boolean shouldExit = false;
 
         Scanner scn = new Scanner(System.in);
-        while(! shouldExit){
+        while(!shouldExit){
+
             System.out.println("Geben Sie zwei Mengen ein: ");
+
             Set<Integer> setA = new HashSet<Integer>();
             Set<Integer> setB = new HashSet<Integer>();
+
             String takeIt = "";
             boolean isInputValid = true;
+
             try {
                 takeIt = scn.nextLine();
             }catch(Exception e){
                 isInputValid = false;
                 System.out.println("Invalid Input: " +e.getMessage());
             }
+
             if(takeIt == "" && isInputValid){
                 shouldExit = true;
             }
 
-            takeIt = takeIt.trim();
-            Pattern pattern = Pattern.compile("\\[([0-9,]*)\\]([+\\\\*-])\\[([0-9,]*)\\]");
+            takeIt = takeIt.trim(); //Cuts off spaces
+            Pattern pattern = Pattern.compile("\\[([0-9,]*)]([+\\\\*-])\\[([0-9,]*)]");
             Matcher matcher = pattern.matcher(takeIt);
-            boolean valid = m.matches();
-            if(valid){
                 String firstSet = "";
-                String Operator = "";
+                String operator = "";
                 String secondSet = "";
                 while(matcher.find()) {
                     firstSet = matcher.group(1);
-                    Operator = matcher.group(2);
+                    operator = matcher.group(2);
                     secondSet = matcher.group(3);
                 }
                 System.out.println("firstSet: " + firstSet);
-                System.out.println("Operator: " + Operator);
+                System.out.println("Operator: " + operator);
                 System.out.println("secondSet: " + secondSet);
-                Operationen ops= new Operationen();
 
-
-            }else{
-                System.out.println("Input does not match the pattern.");
-            }
+                setA.add(firstSet); //Muss noch String -> Int converten und dann zu Set adden bzw converten
+                setB.add(secondSet);
+                switch(operator){
+                    case "+":
+                        System.out.println(vereinigungsMenge(setA, setB));
+                        break;
+                    case "*":
+                        System.out.println(schnittMenge(setA, setB));
+                        break;
+                    case "-":
+                        System.out.println(differenzMenge(setA, setB));
+                        break;
+                }
         }
     }
 }
--- Mengenoperationen/src/Operationen.java
+++ Mengenoperationen/src/Operationen.java
@@ -3,24 +3,24 @@ import java.util.HashSet;
 
 public class Operationen {
 
-        //Schnitt (*)
-        public Set schnittMenge(Set setA, Set setB){
-            Set <Integer> schnitt = new HashSet<Integer>(setA);
-            schnitt.retainAll(setB);
-            return schnitt;
-        }
-        //Differenz (-)
-        public Set differenzMenge(Set setA, Set setB){
-            Set <Integer> differenz = new HashSet<Integer>(setA);
-            differenz.removeAll(setB);
-            return differenz;
-        }
-        //Vereinigung (+)
-        public Set vereinigungsMenge(Set setA, Set setB){
-            Set <Integer> vereinigung = new HashSet<Integer>(setA);
-            vereinigung.addAll(setB);
-            return vereinigung;
-        }
+    //Schnitt (*)
+    public Set schnittMenge(Set setA, Set setB){
+        Set <Integer> schnitt = new HashSet<Integer>(setA);
+        schnitt.retainAll(setB);
+        return schnitt;
+    }
+    //Differenz (-)
+    public Set differenzMenge(Set setA, Set setB){
+        Set <Integer> differenz = new HashSet<Integer>(setA);
+        differenz.removeAll(setB);
+        return differenz;
+    }
+    //Vereinigung (+)
+    public Set vereinigungsMenge(Set setA, Set setB){
+        Set <Integer> vereinigung = new HashSet<Integer>(setA);
+        vereinigung.addAll(setB);
+        return vereinigung;
+    }
 
 
 }
